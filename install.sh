a6c6f6e22d457fe233692ed4511e6196f56420b9
Koljasha <Koljasha@users.noreply.github.com>
2017-04-14 00:26:43 +0300

    Rebase commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# create dirs
+mkdir -p ~/.fonts
+mkdir -p ~/.config/i3
+
+# move files
+mv fonts/* ~/.fonts/
+mv images/.W* ~/Изображения/
+mv config/* ~/.config/i3/
+
+# del install dir
+cd ..
+rm -rf i3wm/
+
+echo
+echo "i3wm config files installed"
+echo
+
