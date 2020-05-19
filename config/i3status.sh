358cd7766f8ecd60ff4a312b52109f4efcd7595d
Koljasha <koljasha@mail.ru>
2020-05-19 20:16:12 +0300

    i3status.sh

--- config/i3status.sh
+++ config/i3status.sh
@@ -1,9 +1,42 @@
 # script i3status extended by Koljasha
 
-i3status -c ~/.config/i3/i3status.conf | (read line && echo "$line" && read line && echo "$line" && read line && echo "$line" &&  while :
-do
-    memory=`free -h | awk '{print "", $3, "/", $2}' | sed -n 2p`
-    lang=`~/.config/i3/language.sh`
-    read line
-    echo ",[ { \"full_text\":\"${lang}\",\"color\":\"#00ff00\" }, { \"full_text\":\"${memory}\",\"color\":\"#00ffff\" }, ${line#,\[}" || exit 1
-done)
+i3status -c ~/.config/i3/i3status.conf |
+(
+    read line && echo "$line" &&
+    read line && echo "$line" &&
+    read line && echo "$line" &&
+    while true
+
+    do
+        memory=`free -h | awk '{print "", $3, "/", $2}' | sed -n 2p`
+        language=`~/.config/i3/language.sh`
+
+        read line
+
+        echo ",[
+            { \"full_text\":\"$language\",\"color\":\"#00ff00\" },
+            { \"full_text\":\"$memory\",\"color\":\"#00ffff\" },
+            ${line#,\[}
+            " || exit 1
+    done
+)
+
+exit 0
+
+###     Для понимания после do:
+###     аналогично выше + дублируем memory после i3status
+
+    do
+        memory=`free -h | awk '{print "", $3, "/", $2}' | sed -n 2p`
+        language=`~/.config/i3/language.sh`
+
+        read line
+        status=`echo "$line" | cut -c 3- | rev | cut -c 2- | rev`
+
+        echo ",[
+            { \"full_text\":\"$language\",\"color\":\"#00ff00\" },
+            { \"full_text\":\"$memory\",\"color\":\"#00ffff\" },
+            $status,
+            { \"full_text\":\"$memory\",\"color\":\"#00ffff\" }
+            ]" || exit 1
+    done
