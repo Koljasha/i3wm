35fb3a8afda474e187ee97fedc670a7178e19eda
Koljasha <koljasha@mail.ru>
2020-12-26 15:35:43 +0300

    polybar brightness change and set

--- polybar/change_brightness.sh
+++ polybar/change_brightness.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+#
 # show:
-# $change_brightness
+# $ ./change_brightness
+#
 # change (after 1 only min 0.1):
-# $ change_brightness +0.05 or $ change_brightness -0.05
+# $ ./change_brightness +0.05 or $ ./change_brightness -0.05
+#
+# set brightness:
+# ./change_brightness 0.80
+#
 
 brightness=`xrandr --verbose | grep -i brightness | cut -d':' -f2 | cut -c2-`
 
@@ -21,6 +27,8 @@ if [[ "$operator" == "+" ]]; then
     brightness=`echo "$brightness $value" | awk '{ print $1+$2 }'`
 elif [[ "$operator" == "-" ]]; then
     brightness=`echo "$brightness $value" | awk '{ print $1-$2 }'`
+else
+    brightness=`echo $params`
 fi
 
 xrandr --output eDP-1 --brightness $brightness
