d32b89f37d5a6a625fe3f91890cd42ad241c03b0
Koljasha <koljasha@mail.ru>
2021-01-08 11:09:57 +0300

    ranger : brightness

--- polybar/change_brightness.sh
+++ polybar/change_brightness.sh
@@ -4,6 +4,30 @@
 # show:
 # $ ./change_brightness
 #
+# set brightness (round):
+# ./change_brightness change
+#
+
+brightness=`xrandr --verbose | grep -i brightness | cut -d':' -f2 | cut -c2- | awk '{ print $1*100 }'`
+if (( $# == 0 )); then
+    echo "$brightness%"
+    exit 0
+fi
+
+if [[ $1 == 'change' ]]; then
+    brightness=`zenity --scale --title="Яркость дисплея" --text="Установите яркость дисплея %" --value=$brightness --step=10 --min-value=10`
+    if (( $? == 0 )); then
+        brightness=`echo $brightness | awk '{ print $1/10 }' | awk '{ print ($0-int($0)<0.499)?int($0):int($0)+1 }' | awk '{ print $1/10 }'`
+        xrandr --output eDP-1 --brightness $brightness
+    fi
+fi
+
+########################################
+
+#
+# show:
+# $ ./change_brightness
+#
 # change (after 1 only min 0.1):
 # $ ./change_brightness +0.05 or $ ./change_brightness -0.05
 #
@@ -11,25 +35,25 @@
 # ./change_brightness 0.80
 #
 
-brightness=`xrandr --verbose | grep -i brightness | cut -d':' -f2 | cut -c2-`
+# brightness=`xrandr --verbose | grep -i brightness | cut -d':' -f2 | cut -c2-`
 
-if (( $# == 0 )); then
-    echo $brightness | awk '{ print $1*100"%" }'
-    exit 0
-fi
+# if (( $# == 0 )); then
+    # echo $brightness | awk '{ print $1*100"%" }'
+    # exit 0
+# fi
 
-params=`echo $1`
+# params=`echo $1`
 
-operator=`echo $params | cut -c1`
-value=`echo $params | cut -c2-`
+# operator=`echo $params | cut -c1`
+# value=`echo $params | cut -c2-`
 
-if [[ "$operator" == "+" ]]; then
-    brightness=`echo "$brightness $value" | awk '{ print $1+$2 }'`
-elif [[ "$operator" == "-" ]]; then
-    brightness=`echo "$brightness $value" | awk '{ print $1-$2 }'`
-else
-    brightness=`echo $params`
-fi
+# if [[ "$operator" == "+" ]]; then
+    # brightness=`echo "$brightness $value" | awk '{ print $1+$2 }'`
+# elif [[ "$operator" == "-" ]]; then
+    # brightness=`echo "$brightness $value" | awk '{ print $1-$2 }'`
+# else
+    # brightness=`echo $params`
+# fi
 
-xrandr --output eDP-1 --brightness $brightness
+# xrandr --output eDP-1 --brightness $brightness
 
